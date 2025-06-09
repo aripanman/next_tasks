0219c71c8a01fd35c5d7ec0f1be0f94f0a86f083
aripanman <ari01sa30@gmail.com>
2025-06-10 00:15:37 +0900

    NavListまで

---
+++ src/components/SideMenu/NavList/NavItem/NavItem.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+interface NavItemProps {
+  label: string;
+  link: string;
+  icon: React.ReactNode;
+}
+const NavItem: React.FC<NavItemProps> = ({ label, link, icon }) => {
+  return (
+    <Link href={link}>
+      <div>{icon}</div>
+      <div>{label}</div>
+    </Link>
+  );
+};
+
+export default NavItem;
